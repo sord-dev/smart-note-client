59a52920e4ec618700a4d834939cd3feb2716504
sord-dev <75338985+sord-dev@users.noreply.github.com>
2023-07-06 19:54:37 +0100

    urh

--- src/utils/api.config.js
+++ src/utils/api.config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000';
+const baseURL = process.env.NEXT_PUBLIC_API_URL || 'https://smart-note.onrender.com';
 
 const api = axios.create({
     baseURL,
